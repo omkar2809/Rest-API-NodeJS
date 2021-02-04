19399de4c2803b6e30ee3989b8048cbba85ec2be
omkar2809 <zemseom10@gmail.com>
2021-02-04 12:26:40 +0530

    socket.io

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const multer = require('multer')
 
 const feedRoutes = require('./routes/feed')
 const authRoutes = require('./routes/auth')
+const { Socket } = require('socket.io')
 
 const app = express()
 
@@ -37,7 +38,10 @@ app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', '*')
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    )
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     next()
 })
@@ -56,6 +60,10 @@ app.use((err, req, res, next) => {
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
-    app.listen(process.env.PORT || 3000)
-})
+    const server = app.listen(process.env.PORT || 3000)
+    const io = require('./socket').init(server)
+    io.on('connection', socket => {
+        console.log('Client connected')
+    })
+}) 
 .catch(err => console.log(err))
--- controllers/feed.js
+++ controllers/feed.js
@@ -4,26 +4,46 @@ const path = require('path')
 const Post = require('../models/post')
 const User = require('../models/user')
 
+const io = require('../socket')
+
 const { validationResult } = require('express-validator')
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
     const currentPage = req.query.page || 1
     const perPage = 2
-    let totalItems
-    Post.countDocuments()
-    .then(count => {
-        totalItems = count
-        return Post.find().skip((currentPage - 1) * perPage).limit(perPage)
-    })
-    .then(posts => {
+    // let totalItems
+    try {
+        const totalItems = await Post.countDocuments()
+    
+        const posts = await Post.find()
+                                .populate('creator')
+                                .sort({createdAt: -1})
+                                .skip((currentPage - 1) * perPage)
+                                .limit(perPage)
+        
         res.status(200).json({message: 'Fetched Post Successfully', posts: posts, totalItems: totalItems})
-    })
-    .catch(err => {
+    }
+    catch(err) {
         if (!err.statusCode) {
             err.statusCode = 500
         }
         next(err)
-    })
+    }
+    
+    // Post.countDocuments()
+    // .then(count => {
+    //     totalItems = count
+    //     return Post.find().skip((currentPage - 1) * perPage).limit(perPage)
+    // })
+    // .then(posts => {
+    //     res.status(200).json({message: 'Fetched Post Successfully', posts: posts, totalItems: totalItems})
+    // })
+    // .catch(err => {
+        // if (!err.statusCode) {
+        //     err.statusCode = 500
+        // }
+        // next(err)
+    // })
 }
 
 exports.createPost = (req, res, next) => {
@@ -58,6 +78,7 @@ exports.createPost = (req, res, next) => {
         return user.save()
     })
     .then(result => {
+        io.getIO().emit('posts', { action: 'create', post: {...post._doc, creator: { _id: req.userId, name: creator.name}} })
         res.status(200).json({
             message: 'Post created successfully', 
             post: post, 
@@ -113,14 +134,14 @@ exports.updatePost = (req, res, next) => {
         error.statusCode(422)
         throw error
     }
-    Post.findById(postId)
+    Post.findById(postId).populate('creator')
     .then(post => {
         if(!post) {
             const error = new Error('Could not find post.')
             error.statusCode = 404
             throw error
         }
-        if (post.creator.toString() !== req.userId.toString()) {
+        if (post.creator._id.toString() !== req.userId.toString()) {
             const error = new Error('Not Authorized.')
             error.statusCode = 403
             throw error
@@ -134,6 +155,7 @@ exports.updatePost = (req, res, next) => {
         return post.save()
     })
     .then(result => {
+        io.getIO().emit('posts', { action: 'update', post: result})
         res.status(200).json({
             message: 'Post updated',
             post: result
@@ -180,6 +202,7 @@ exports.deletePost = (req, res, next) => {
     })
     .then(result => {
         console.log(result)
+        io.getIO().emit('posts', {action: 'delete', post: postId})
         res.status(200).json({
             message: 'Post deleted'
         })
